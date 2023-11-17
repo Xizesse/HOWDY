05254494120709f97474014818b02c61b14a1b16
up202005921 <up202005921@up.pt>
2023-11-17 18:41:56 +0000

    creation of net package for multiplayer communication.

---
+++ Client/src/net/GameClient.java
@@ -0,0 +1,14 @@
+package net;
+
+import java.net.InetAddress;
+import java.net.DatagramSocket;
+
+import main.GamePanel;
+
+public class GameClient extends Thread{
+    private InetAddress ipAddress;
+    private DatagramSocket socket;
+    private GamePanel game;
+
+    //public GameClient()
+}
