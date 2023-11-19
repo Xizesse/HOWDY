4e71c5c0c8744478044e7cf699dd834c2a8db7a3
Érico Dias <up201900211@up.pt>
2023-11-19 21:22:06 +0000

    Typo fixed

--- Client/src/entity/Entity.java
+++ Client/src/entity/Entity.java
@@ -20,7 +20,7 @@ public class Entity {
     public int spriteCounter = 0;
     public int spriteNum = 1;
     public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
-    public boolean colisionOn = false;
+    public boolean collisionOn = false;
 
     public Entity(GamePanel gp) {
         this.gp = gp;
