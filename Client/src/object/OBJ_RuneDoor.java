a7b68192f262301f2aa5e711142a12dbea1c4992
Érico Dias <up201900211@up.pt>
2023-12-17 23:26:09 +0000

    RuneDoor bug fix

--- Client/src/object/OBJ_RuneDoor.java
+++ Client/src/object/OBJ_RuneDoor.java
@@ -4,6 +4,7 @@ import main.GamePanel;
 import net.Packet06MapChange;
 import net.TileChange;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class OBJ_RuneDoor extends SuperObject{
@@ -14,8 +15,8 @@ public OBJ_RuneDoor(GamePanel gp, char shape) {
         type = "door";
         this.shape = shape;
         equippable = false;
-        collision = true;
         this.gp = gp;
+        this.solidArea = new Rectangle(-1,-1,50,50);
 
     }
 
@@ -25,7 +26,6 @@ public OBJ_RuneDoor(GamePanel gp, char shape) {
         for (SuperObject obj : gp.player.inventory) {
             if (obj instanceof OBJ_Rune) {
                 if (((OBJ_Rune) obj).shape == this.shape) {
-                    this.collision = false;
                     ArrayList<TileChange> changes = new ArrayList<>();
                     TileChange change;
                     change = new TileChange(gp.currentMap, this.worldX /gp.tileSize, this.worldY/gp.tileSize, 50);
