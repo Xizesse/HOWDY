12ba0f862ed742e19ae2fe8f2dbfa4de4bcc3e22
Érico Dias <up201900211@up.pt>
2023-12-19 10:59:23 +0000

    EventHandlerTest

---
+++ Client/src/test/EventHandlerTest.java
@@ -0,0 +1,51 @@
+package test;
+
+import entity.Player;
+import main.GamePanel;
+import main.KeyHandler;
+import net.GameClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import main.EventHandler;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventHandlerTest {
+    GamePanel gp;
+    EventHandler eH;
+    @BeforeEach
+    void setUp() throws IOException {
+        gp = new GamePanel();
+        eH = new EventHandler(gp);
+    }
+    @Test
+    void hitTest() {
+        gp.player = new Player(gp, new KeyHandler(gp),10, 3, 0 );
+        assertTrue(eH.hit(0, 10, 3, "any"));
+        assertFalse(eH.hit(0, 30, 15, "any"));
+    }
+
+    @Test
+    void dmgPitTest() {
+        this.gp.socketClient = new GameClient(gp, "localhost");
+        gp.player = new Player(gp, new KeyHandler(gp),0, 5, 5 );
+        int health = gp.player.currentHealth;
+        eH.dmgPit(gp.playState);
+        assertEquals(health-1, gp.player.currentHealth );
+    }
+
+    @Test
+    void teleportTest() {
+        Player player = new Player(gp, new KeyHandler(gp),0, 0, 0 );
+        player.worldX = 10;
+        player.worldY = 30;
+        eH.teleport( gp.playState, 1, 1, 15);
+        assertEquals(1, gp.currentMap);
+        assertEquals(10, player.worldX);
+        assertEquals(30, player.worldY);
+    }
+
+
+}
